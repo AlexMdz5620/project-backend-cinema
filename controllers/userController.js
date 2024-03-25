981030b3dc7a6f3b5513c5ac8b4e53273a58d924
Alex Mdz <ichialex5620mail.com>
2024-03-24 21:03:56 -0600

    Agregando las rutas de usuarios

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,31 @@
 import User from '../models/User.js'
 
+// get all users
+const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await User.find({}, { password: 0 })
+    res.json(allUsers)
+  } catch (error) {
+    res.status(500).json({
+      msg: 'Error finding users',
+      error
+    })
+  }
+}
 // get user by id
 const getUserById = async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId)
-    res.json(user)
+    const getUserById = await User.findById(req.params.userId, { password: 0 })
+    if (getUserById) {
+      return res.status(200).json({
+        msg: 'User found successfully',
+        user: getUserById
+      })
+    } else {
+      res.status(404).json({
+        msg: 'User not found'
+      })
+    }
   } catch (error) {
     res.status(500).json({
       msg: 'Error al encontrar el usuario',
@@ -12,34 +33,34 @@ const getUserById = async (req, res) => {
     })
   }
 }
-// get all users
-const getAllUsers = async (req, res) => {
+// get user by role
+const getUserByRole = async (req, res) => {
   try {
-    const allUsers = await User.find()
-    res.json(allUsers)
+    const role = req.params.userRole.toUpperCase()
+    if (!['CUSTOMER'].includes(role)) {
+      return res.status(400).json({
+        msg: 'Invalid role provided'
+      })
+    }
+
+    const usersByRole = await User.find({ role }, { password: 0 })
+
+    if (usersByRole.length > 0) {
+      return res.status(200).json({
+        msg: 'Users found successfully',
+        users: usersByRole
+      })
+    } else {
+      return res.status(404).json({
+        msg: 'No users found with the specified role'
+      })
+    }
   } catch (error) {
     res.status(500).json({
-      msg: 'Error al encontrar usuarios',
-      error
-    })
-  }
-}
-// update user
-const updateUser = async (req, res) => {
-  try {
-    const { userId } = req.params
-    const updateUser = await User.updateOne({
-      _id: userId
-    },
-    req.body
-    )
-    res.json(updateUser)
-  } catch (error) {
-    res.status(500).json({
-      msg: 'Error al actualizar el usuario',
+      msg: 'Error finding users',
       error
     })
   }
 }
 
-export { getUserById, getAllUsers, updateUser }
+export { getUserById, getAllUsers, getUserByRole }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express'
-import { getUserById, getAllUsers, updateUser } from '../controllers/userController.js'
+import { getUserById, getAllUsers, getUserByRole } from '../controllers/userController.js'
 import { authValidator } from '../middlewares/authValidator.js'
 import { isAdmin } from '../middlewares/isAdmin.js'
+import { isEmployee } from '../middlewares/isEmployee.js'
 
 const userRoute = express.Router()
 
-userRoute.route('/').get(authValidator, isAdmin, getAllUsers)
-userRoute.route('/:userId').get(authValidator, getUserById).put(authValidator, updateUser)
-
+userRoute.route('/')
+  .get(authValidator, isEmployee, getAllUsers)
+userRoute.route('/:userId')
+  .get(authValidator, isAdmin, getUserById)
+userRoute.route('/role/:userRole')
+  .get(getUserByRole)
 export default userRoute
